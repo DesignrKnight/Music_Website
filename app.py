fd0443746a163d5cce6b397db9620c7d77fc9456
PraisySam <praisysam24@gmail.com>
2020-08-16 17:06:13 +0530

    Fetch API in Frontend

--- app.py
+++ app.py
@@ -55,20 +55,22 @@ def add_product():
   db.session.commit()
 
   return product_schema.jsonify(new_product)
+  
 
 # Get All Products
 @app.route('/product', methods=['GET'])
 def get_products():
   all_products = Product.query.all()
   result = products_schema.dump(all_products)
-  return jsonify(result)
+  return render_template('playlist.html', title="Products" ,  result = result )
+
 
 # Get Single Products
 @app.route('/product/<id>', methods=['GET']) 
 def get_product(id):
   product = Product.query.get(id)
-  return product_schema.jsonify(product)
-
+  return render_template('song.html', title="Product(ID)" ,  product =product )
+     
 # Update a Product
 @app.route('/product/<id>', methods=['PUT'])
 def update_product(id):
@@ -85,7 +87,6 @@ def update_product(id):
   product.album = album
 
   db.session.commit()
-
   return product_schema.jsonify(product)
 
 # Delete Product
